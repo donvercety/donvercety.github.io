29c75ae065d86d2d027af5ee64b5696b31dc423a
Tommy Vercety <donvercety@gmail.com>
2015-08-31 07:24:41 +0000

    myCookie.js refactor

--- js/myCookie.js
+++ js/myCookie.js
@@ -1,46 +1,37 @@
 var myCookie = {
-    createCookie: function(name, value, days)
-    {
+    createCookie: function(name, value, days) {
         var expires;
 
-        if (days)
-        {
+        if (days) {
             var date = new Date();
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toGMTString();
-        }
-        else
-        {
+        } else {
             expires = "";
         }
-
         document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
     },
 
-    readCookie: function(name)
-    {
+    readCookie: function(name) {
+        
         var nameEQ = escape(name) + "=";
         var ca     = document.cookie.split(';');
 
-        for (var i = 0; i < ca.length; i++)
-        {
+        for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
 
-            while (c.charAt(0) === ' ')
-            {
+            while (c.charAt(0) === ' ') {
                 c = c.substring(1, c.length);
             }
 
-            if (c.indexOf(nameEQ) === 0)
-            {
+            if (c.indexOf(nameEQ) === 0) {
                 return unescape(c.substring(nameEQ.length, c.length));
             }
         }
         return null;
     },
 
-    eraseCookie: function(name)
-    {
+    eraseCookie: function(name) {
         return this.createCookie(name, "", -1);
     }
 };
